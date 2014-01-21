92b24bca65b9e09de8317699fb8d3783da2756c3
runememborg <memborg@customweb.dk>
2014-01-21 18:36:08 +0100

    Better time handling
    Instead of updating the time in realtime just calculate the elapsed time
    between click on an button either on it self, another button or creating
    a new button

--- timer.js
+++ timer.js
@@ -1,32 +1,51 @@
 'use strict';
-var _index = 0;
-var _interval = null;
-var _curIndex = -1;
-var _wait = 1000*60;
+var _oneMinute = 60*1000;
 var _totalTime = 0;
 var _elm = null;
 var _elmTotalTime = null;
-
-var UpdateButton = function(){
-    var timer = +_elm.timer;
-    timer++;
-    _totalTime++;
-    var text = _elm.title + ': ' + timer + 'min';
-    _elm.timer = timer;
-    _elm.innerHTML = text;
-    _elmTotalTime.innerHTML = 'Total: ' + _totalTime + 'min';
+var log = function(){
+    if (typeof(console) !== "undefined" && console.log !== undefined){
+        try{
+            console.log.apply(console, arguments);
+        }
+        catch (e){
+            var log = Function.prototype.bind.call(console.log, console);
+            log.apply(console, arguments);
+        }
+    }
 };
 
-var StartButton = function(){
-    clearInterval(_interval);
-    var index = this.index;
+var ToggleButton = function(){
+    var starttime = 0;
+    var now = new Date().getTime();
+    var elapsed = 0;
+    var days = 0;
+    var timer = 0;
     if(_elm !== null){
         _elm.style.fontWeight = 'normal';
+        timer = +_elm.timer;
+        starttime = _elm.started.getTime();
+        elapsed = Math.abs((now - starttime) / _oneMinute);
+        days = Math.round(elapsed);
+        _elm.timer = timer + days;
+        _elm.innerHTML = _elm.title + ': ' + _elm.timer + 'min';
+    }else if(_elm === this){
+        timer = +_elm.timer;
+        starttime = _elm.started.getTime();
+        elapsed = Math.abs((now - starttime) / _oneMinute);
+        days = Math.round(elapsed);
+        _elm.timer = timer + days;
+        _elm.innerHTML = _elm.title + ': ' + _elm.timer + 'min';
     }
-    _curIndex = +index;
-    _elm = document.getElementById('button' + _curIndex);
+
+    _totalTime += days;
+    _elmTotalTime.innerHTML = 'Total: ' + _totalTime + 'min';
+
+    _elm = this;
+    _elm.started = new Date();
+    var active = +_elm.active;
+    active++;
     _elm.style.fontWeight = 'bold';
-    _interval = setInterval(UpdateButton, _wait);
 };
 
 var AddTimer = function(){
@@ -38,26 +57,25 @@ var AddTimer = function(){
 
     var button = document.createElement('button');
     button.setAttribute('type','button');
-    button.id = 'button' + _index;
-    button.index = _index;
     button.title = title;
-    button.innerHTML = title + ': 0min';
+    button.innerHTML = title;
     button.timer = 0;
-    button.onclick = StartButton;        
+    button.active = 0;
+    button.started = new Date();
+    button.onclick = ToggleButton;        
     button.style.backgroundColor = 'rgb('+r+','+g+','+b+')';
-    if(sum < 300){
-        button.style.color= '#fff';
-    }
-    _curIndex = _index;
-    _index++;
-    document.body.appendChild(button);
-    button.click();
-};
+            if(sum < 300){
+                button.style.color= '#fff';
+            }
+            document.body.appendChild(button);
+            button.click();
+            };
 
-var Init = function(){
-    var button = document.getElementById('jsnewtimer');
-    button.onclick = AddTimer;
-    _elmTotalTime = document.getElementById('jstotaltime');
-};
+            var Init = function(){
+                var button = document.getElementById('jsnewtimer');
+                button.onclick = AddTimer;
+                _elmTotalTime = document.getElementById('jstotaltime');
+            };
 
-Init();
+            Init();
+
